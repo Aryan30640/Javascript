2851dfab330fc93a86aec4239cf6d05a0494ea98
aryan30640 <aryan30640@gmail.com>
2024-11-17 23:14:38 +0530

    Updated

---
+++ evod.js
@@ -0,0 +1,7 @@
+let num = 1;
+let mod = num%2;
+if(mod === 0) {
+    console.log("Even")
+} else {
+    console.log("Odd")
+}
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+//--
 console.log('Hi!')
 let a = 2;
 let b = 4;
-
-
 let cond1 = a > b; //false
 let cond2 = a < b; //true
-
-
 console.log(cond1 && cond2); //dono sahi honi chahiye warna false
 console.log(cond1 || cond2); //dono me se 1 bhi sahi hua to true
 console.log(!(a === b)); //opposite answer deta hai ye.
 
-
+//--
 age = 24;
 if(age > 18) {
     console.log("You can vote because you are more than 18 years old.");
@@ -20,8 +17,16 @@ if(age < 18) {
     console.log("You cannot vote as you are minor")
 }
 
-
-
+//--
+let mode = "dark";
+let color;
+if(mode === "dark") {
+    color = "black";
+}
+if(mode === "light") {
+    color = "white";
+}
+console.log(color);
 
 
 
