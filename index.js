e21528a1bfef991083bc820beff6fbc9d68c72b6
aryan30640 <aryan30640@gmail.com>
2024-11-17 22:50:51 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,71 @@
+console.log('Hi!')
+let a = 2;
+let b = 4;
+
+
+let cond1 = a > b; //false
+let cond2 = a < b; //true
+
+
+console.log(cond1 && cond2); //dono sahi honi chahiye warna false
+console.log(cond1 || cond2); //dono me se 1 bhi sahi hua to true
+console.log(!(a === b)); //opposite answer deta hai ye.
+
+
+age = 24;
+if(age > 18) {
+    console.log("You can vote because you are more than 18 years old.");
+}
+if(age < 18) {
+    console.log("You cannot vote as you are minor")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
